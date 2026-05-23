c31d2670382bddc25e48e56ecf59f266fb5737de
Sanjeev Kumar <sanjeev2856@gmail.com>
2026-05-23 16:44:27 +0530

    Added extension methods for ordering, catalog and basket modules and added them as service for application
    Moved
    <ItemGroup>
        <PackageReference Include="Microsoft.Extensions.Configuration.Abstractions" Version="10.0.8" />
        <PackageReference Include="Microsoft.Extensions.DependencyInjection.Abstractions" Version="10.0.8" />
      </ItemGroup>
    to shared to avoid installing these packages for each separate module

--- src/Bootstrapper/API/Program.cs
+++ src/Bootstrapper/API/Program.cs
@@ -1,6 +1,20 @@
+using Catalog;
+using Ordering;
+using Basket;
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+//builder.Services.AddControllers();
+
+builder.Services
+    .AddCatalogModule(builder.Configuration)
+    .AddBasketModule(builder.Configuration)
+    .AddOrderingModule(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+
 
 app.Run();
---
+++ src/Modules/Ordering/Ordering/OrderModule.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace Ordering;
+
+public static class OrderModule
+{
+      public static IServiceCollection AddOrderingModule(this IServiceCollection services, IConfiguration configuration )
+    {
+        return services;
+    }
+}
