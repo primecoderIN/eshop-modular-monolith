6a60e0e2281c9307a8c36f6748754ece1014f4e2
Sanjeev Kumar <sanjeev2856@gmail.com>
2026-05-23 22:31:34 +0530

    Added shared Domain event interface, Shared Entity interface etc

---
+++ src/Shared/Shared/DDD/Entity.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Shared.DDD;
+
+public abstract class Entity<T> : IEntity<T>
+{
+    public abstract T Id { get; set; }
+    public abstract DateTime? CreatedAt { get; set; }
+    public abstract string? CreatedBy { get; set; }
+    public abstract DateTime? LastModified { get; set; }
+    public abstract string? LastModifiedBy { get; set; }
+ 
+}
---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+namespace Shared.DDD;
+
+public interface IDomainEvent : INotification //Comes from MediatR, we will use it to publish domain events
+{
+     Guid EventId => Guid.NewGuid();
+
+    public DateTime OccuredOn => DateTime.Now;
+
+    public string EventType => GetType().AssemblyQualifiedName!;
+}
