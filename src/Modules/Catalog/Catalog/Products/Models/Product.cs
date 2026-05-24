22f45b19b96656cfdb7800133256cb930ca4983f
Sanjeev Kumar <sanjeev2856@gmail.com>
2026-05-24 16:16:40 +0530

    Created product model in Catalog module along with Create and Update method

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Catalog.Products.Models;
+
+internal class Product : Entity<Guid>
+{
+    public string Name { get;private set; } = default!;
+
+    public List<string> Category { get; private set; } = new();
+
+    public string Description { get;private set; } = default!;
+
+    public string ImageFile { get; private set; } = default!;
+
+    public decimal Price { get; private set; }
+
+    //Create is static because here no objeect is created yet. 
+    public static Product Create(Guid id, string name, List<string>category, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentNullException.ThrowIfNull(category);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        return new Product
+        {
+            Id = id,
+            Name = name,
+            Category = category,
+            Description = description,
+            ImageFile = imageFile,
+            Price = price
+        };
+
+    }
+
+    public void Update(string name, List<string> category, string description, string imageFile, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(name);
+        ArgumentNullException.ThrowIfNull(category);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+        Name = name;
+        Category = category;
+        Description = description;
+        ImageFile = imageFile;
+        Price = price;
+    }
+
+}
--- src/Shared/Shared/DDD/Entity.cs
+++ src/Shared/Shared/DDD/Entity.cs
@@ -4,10 +4,10 @@ namespace Shared.DDD;
 
 public abstract class Entity<T> : IEntity<T>
 {
-    public abstract T Id { get; set; }
-    public abstract DateTime? CreatedAt { get; set; }
-    public abstract string? CreatedBy { get; set; }
-    public abstract DateTime? LastModified { get; set; }
-    public abstract string? LastModifiedBy { get; set; }
+    public  T Id { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public  string? CreatedBy { get; set; }
+    public  DateTime? LastModified { get; set; }
+    public  string? LastModifiedBy { get; set; }
  
 }
